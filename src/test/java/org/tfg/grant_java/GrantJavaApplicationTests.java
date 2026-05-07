7c373e2950a0b4bed7ce173c364ce4de20e2dbb2
Fosker <stuart.fosker@lloydsbanking.com>
2026-05-07 07:43:18 +0100

    Initial commit

---
+++ src/test/java/org/tfg/grant_java/GrantJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tfg.grant_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrantJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
